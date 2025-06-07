78a5b073b0dd96af7b1116c86da576d76c295052
mohammedabudi <110717596+mohammedabudi@users.noreply.github.com>
2025-06-07 18:47:57 +0300

    index.js

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-const { initializeApp } = require("firebase/app");
-const { getFirestore, collection, addDoc } = require("firebase/firestore");
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBJmSb9l0LHZgGQKv2ZEZc_DJLvFrPpmSg",
-  authDomain: "trainingfeedback-f788d.firebaseapp.com",
-  projectId: "trainingfeedback-f788d",
-  storageBucket: "trainingfeedback-f788d.appspot.com",
-  messagingSenderId: "509836635886",
-};
-
-const firebaseApp = initializeApp(firebaseConfig); 
-const db = getFirestore(app);
-
-module.exports = { db, collection, addDoc };
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
