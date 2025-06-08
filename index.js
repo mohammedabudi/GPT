ee98d971ab8610b50b1eaf3cdcd9e793a55c0802
mohammedabudi <110717596+mohammedabudi@users.noreply.github.com>
2025-06-08 14:03:03 +0300

    index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const { db, collection, addDoc } = require('./firebase.js');
+const { doc, getDoc, setDoc } = require("firebase/firestore");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
